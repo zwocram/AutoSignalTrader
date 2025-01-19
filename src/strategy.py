73d1f8346df894a426a95858f7496c71bdd77fe3
zwocram <marco.zwan@proton.me>
2025-01-19 23:11:42 +0100

    changed __ini__ params - 2

--- src/strategy.py
+++ src/strategy.py
@@ -1,8 +1,9 @@
 
 
 class Strategy:
-    def __init__(self, risklevel, portfolioheat, select_tp_level):
+    def __init__(self, risklevel, portfolioheat, select_tp_level, select_tp_level_explanation):
         self.risklevel = risklevel
         self.portfolioheat = portfolioheat
         self.tpLevel = select_tp_level
+        self.tpLevelExplanation = select_tp_level_explanation
 
